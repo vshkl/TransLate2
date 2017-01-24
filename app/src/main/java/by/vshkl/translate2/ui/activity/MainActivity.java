0151b9b64a8848a609730833a2a3a5f4f7894acb
Pavel Vashkel <jgugn1@gmail.com>
2017-01-24 16:07:02 +0300

    Update map overlay ui

--- app/src/main/java/by/vshkl/translate2/ui/activity/MainActivity.java
+++ app/src/main/java/by/vshkl/translate2/ui/activity/MainActivity.java
@@ -5,13 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
+import butterknife.OnClick;
 import by.vshkl.translate2.R;
 
 public class MainActivity extends MvpAppCompatActivity implements OnMapReadyCallback {
+
+    private static final float ZOOM_CITY = 11F;
+    private static final float ZOOM_STREET = 11F;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +28,16 @@ public class MainActivity extends MvpAppCompatActivity implements OnMapReadyCall
 
     //------------------------------------------------------------------------------------------------------------------
 
+    @OnClick(R.id.fb_location)
+    void onLocationClicked() {
+
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(53.9024429, 27.5614649), ZOOM_CITY));
     }
 
     //------------------------------------------------------------------------------------------------------------------
